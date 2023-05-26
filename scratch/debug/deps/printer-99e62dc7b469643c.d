4c2d5ed38de0365f34ca86f0b609682e694c4b27
gpwclark <gpwclark@users.noreply.github.com>
2023-05-26 19:11:16 +0000

    Deploying to gh-pages from @ gpwclark/github-action-play@b3a84595fe3948f8abd96e6ae4e23daf57fb5246 🚀

---
+++ scratch/debug/deps/printer-99e62dc7b469643c.d
@@ -0,0 +1,5 @@
+/home/runner/work/github-action-play/github-action-play/docs/scratch/debug/deps/printer-99e62dc7b469643c.rmeta: printer/src/lib.rs
+
+/home/runner/work/github-action-play/github-action-play/docs/scratch/debug/deps/printer-99e62dc7b469643c.d: printer/src/lib.rs
+
+printer/src/lib.rs:
---
+++ scratch/doc/crates.js
@@ -0,0 +1,1 @@
+window.ALL_CRATES = ["printer","scratch"];
